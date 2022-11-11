b2462fd84350d04e7bc4f5f7e588a91cca7e5211
Gabriel Castillo <castillogabriel@uma.es>
2022-11-11 17:46:02 +0100

    login and sign up views finished

--- AndroidEmail/app/src/main/java/com/example/android_email/Activities/SignInActivity.java
+++ AndroidEmail/app/src/main/java/com/example/android_email/Activities/SignInActivity.java
@@ -16,8 +16,6 @@ public class SignInActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sing_in);
-
         binding = ActivitySingInBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
         setListeners();
--- AndroidEmail/app/src/main/java/com/example/android_email/Activities/SignUpActivity.java
+++ AndroidEmail/app/src/main/java/com/example/android_email/Activities/SignUpActivity.java
@@ -14,9 +14,9 @@ public class SignUpActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sing_up);
         binding = ActivitySingUpBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        setListeners();
 
     }
 
