d50f620ccd36ce4643840be5cbc57c91e5e21aae
Gabriel Castillo <castillogabriel@uma.es>
2022-11-11 15:25:10 +0100

    Sign in and up created
    Both views has being created. Exists nevegability but they dont work yet

---
+++ AndroidEmail/app/src/main/java/com/example/android_email/Activities/SignInActivity.java
@@ -0,0 +1,32 @@
+package com.example.android_email.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.example.android_email.R;
+import com.example.android_email.databinding.ActivitySingInBinding;
+
+public class SignInActivity extends AppCompatActivity {
+
+    private ActivitySingInBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sing_in);
+
+        binding = ActivitySingInBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        setListeners();
+
+    }
+
+    private void setListeners() {
+        binding.createAccount.setOnClickListener(v -> startActivity(new Intent(getApplicationContext(), SignUpActivity.class)));
+
+    }
+
+}
